6498a61658aded452c7f3f34f7b9a02bf1b94762
XDawn66 <jiang1241838337@Gmail.com>
2023-10-24 13:39:05 -0700

    Update ArrayExamples.java

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -26,6 +26,7 @@ public class ArrayExamples {
   // lowest number when calculating. Returns 0 if there are no elements or just
   // 1 element in the array
   static double averageWithoutLowest(double[] arr) {
+    double lowcount = 0;
     if(arr.length < 2) { return 0.0; }
     double lowest = arr[0];
     for(double num: arr) {
@@ -33,9 +34,12 @@ public class ArrayExamples {
     }
     double sum = 0;
     for(double num: arr) {
-      if(num != lowest) { sum += num; } //????
+      if(num != lowest) { sum += num; }
+      else{
+      lowcount++;
+      }
     }
-    return sum / (arr.length - 1);
+    return sum / (arr.length - lowcount);
   }
 
 
